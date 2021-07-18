b5b16737709044e38d98e4992a7595a005b548fc
ZandBraxton <Zand.Braxton@gmail.com>
2021-07-18 14:44:13 -0500

    Finished EaS, added more CSS, added clear and resize buttons

--- scripts.js
+++ scripts.js
@@ -1,24 +1,53 @@
-//I need to make a loop that creates 256 divs, 16 rows of 16 divs that are a square
-//Use grid or flexbox?
-
 const container = document.getElementById('container');
+let r = 16;
+let c = 16;
+const btn = document.querySelector('#reGrid');
+const clear = document.querySelector('#clear')
+const
+clear.addEventListener('click', clearGrid)
+btn.addEventListener('click', reGrid);
+makeRows(r, c);
 
 function makeRows (rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div')
         container.appendChild(cell).className = "grid-item";
     };
+    listen()
 };
 
-
-makeRows(16, 16);
-
-const cell = document.getElementsByClassName('grid-item')
+function listen() {
+    const cell = document.getElementsByClassName('grid-item')
 for (let i = 0; i < cell.length; i++) {
     cell[i].addEventListener('mouseover', function() {
         cell[i].classList.add('color')
     })
+    }
 }
 
+function reGrid () {
+    resetGrid()
+    while (true) {
+        let input = prompt("Select Grid Size (Max 100)")
+        if (input > 0 && input <= 100) {
+            makeRows(input, input)
+            return false
+        }
+    }
+}
+
+function resetGrid () {
+    let cell = document.querySelectorAll('.grid-item')
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].remove()
+    }
+}
+
+function clearGrid() {
+    let cell = document.querySelectorAll('.grid-item')
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].classList.remove('color')
+    }
+}
